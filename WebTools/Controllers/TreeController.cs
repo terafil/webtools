cc00efa91e3d5933479b4f9879c81ba0b0946337
ratherton <ratherton@innovasi.com>
2015-04-06 15:17:01 -0400

    Started creating restfile file post

--- WebTools/Controllers/TreeController.cs
+++ WebTools/Controllers/TreeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
 
@@ -27,6 +29,44 @@ namespace WebTools.Controllers
             };
             System.Threading.Thread.Sleep(1000);
             return Ok(leafs);
+        }
+
+        [Route("api/v1/files")]
+        [HttpPost]
+        public IHttpActionResult PostFile()
+        {
+            if(!Request.Content.IsMimeMultipartContent())
+            {
+                return BadRequest();
+            }
+            string root = HttpContext.Current.Server.MapPath("~/pending");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            try
+            {
+                Request.Content.ReadAsMultipartAsync(provider).Wait();
+
+                /*foreach (MultipartFileData file in provider.FileData)
+                {
+                    Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server file path: " + file.LocalFileName);
+                }*/
+
+                return Ok();
+            }
+            catch(Exception e)
+            {
+                return BadRequest();
+            }
+
+            /*var files = HttpContext.Current.Request.Files;
+
+            foreach(HttpPostedFile file in files)
+            {
+                file.SaveAs(Path.Combine(HttpContext.Current.Server.MapPath("~/pending"), file.FileName));
+            }*/
+
+            
         }
     }
 
